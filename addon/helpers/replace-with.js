4c8a010242df059e7bb3099c3ef762c668da1f7f
Juan Antonio Gomez Benito <juanantonio.gomez.benito@bbva.com>
2017-05-23 11:34:29 +0200

    Replace slice by copy. Tabs to spaces.

--- addon/helpers/replace-with.js
+++ addon/helpers/replace-with.js
@@ -5,20 +5,20 @@ const { getOwner, copy } = Ember;
 
 export default Ember.Helper.extend({
 
-	compute([...params]) {
-		const _params = copy(params);
-		const router = getOwner(this).lookup('router:main');
-		const queryParams = _params[_params.length - 1];
+  compute([...params]) {
+    const _params = copy(params);
+    const router = getOwner(this).lookup('router:main');
+    const queryParams = _params[_params.length - 1];
 
-		if (queryParams instanceof QueryParams) {
-			_params[_params.length - 1] = { queryParams: queryParams.values };
-		}
+    if (queryParams instanceof QueryParams) {
+      _params[_params.length - 1] = { queryParams: queryParams.values };
+    }
 
-		return function(value) {
-			router.replaceWith(..._params);
+    return function(value) {
+      router.replaceWith(..._params);
 
-			return value;
-		};
-	}
+      return value;
+    };
+  }
 
 });
--- addon/helpers/transition-to.js
+++ addon/helpers/transition-to.js
@@ -1,22 +1,25 @@
 import Ember from 'ember';
 import QueryParams from 'ember-route-helpers/utils/query-params';
 
-const { getOwner } = Ember;
+const { getOwner, copy } = Ember;
 
 export default Ember.Helper.extend({
-	compute(params) {
-		params = params.slice();
 
-		const router = getOwner(this).lookup('router:main');
-		const queryParams = params[params.length - 1];
+  compute([...params]) {
+    const _params = copy(params);
+    const owner = getOwner(this);
+    const router = owner.lookup('router:main');
+    const queryParams = _params[_params.length - 1];
 
-		if (queryParams instanceof QueryParams) {
-			params[params.length - 1] = { queryParams: queryParams.values };
-		}
+    if (queryParams instanceof QueryParams) {
+      _params[_params.length - 1] = { queryParams: queryParams.values };
+    }
 
-		return function(value) {
-			router.transitionTo(...params);
-			return value;
-		};
-	}
+    return function(value) {
+      router.transitionTo(..._params);
+
+      return value;
+    };
+  }
+
 });
