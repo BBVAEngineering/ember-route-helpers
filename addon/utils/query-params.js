891010c564c6053cab8e85609584fa7398c98efe
Eugenio Mendoza Robaina <eugenio.mendoza2@bbva.com>
2018-07-11 09:04:08 +0200

    Fixed query-params util

--- addon/utils/query-params.js
+++ addon/utils/query-params.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
-const { QueryParams } = Ember.__loader.require('ember-routing');
+const { QueryParams } = Ember.__loader.require('ember-routing/ib/system/query_params').default;
 
 export default QueryParams;
