e0360bb44fccad65660f52a96e3e70a1f784cf50
Julian Perez <julian.perez@bbva.com>
2018-07-11 11:01:11 +0200

    Backward compatibility between versions

--- addon/utils/query-params.js
+++ addon/utils/query-params.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 const QueryParams = Ember.__loader.require('ember-routing/lib/system/query_params').default;
+const {
+	QueryParamsOldVersions
+} = Ember.__loader.require('ember-routing');
 
-export default QueryParams;
+export default QueryParams || QueryParamsOldVersions;
