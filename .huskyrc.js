e4b103ccc34d5cdd19922df535f92a08610ad67c
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2018-12-05 10:10:31 +0100

    chore(package): add new life cycle

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	hooks: {
-		"commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
-		"pre-push": "npm run lint:js && npm run test"
+		'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+		'pre-push': 'npm run lint:js && npm run lint:hbs && npm run test'
 	}
 };
