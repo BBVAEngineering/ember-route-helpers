1a8609693303c9f3ef92da1f792c113375c3828e
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2018-12-05 13:17:25 +0100

    chore(template-lint): fix some rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,24 +3,7 @@
 module.exports = {
 	extends: 'recommended',
 	rules: {
-		'bare-strings': true,
-		'block-indentation': 'tab',
-		'html-comments': true,
-		'nested-interactive': true,
-		'self-closing-void-elements': true,
-		'triple-curlies': true,
-		'deprecated-each-syntax': true,
-		'link-rel-noopener': true,
-		'invalid-interactive': true,
-		'img-alt-attributes': true,
-		'style-concatenation': true,
-		'deprecated-inline-view-helper': true,
-		'unused-block-params': true,
-		'inline-link-to': true,
-		'inline-styles': true,
-		'attr-indentation': 'tab',
-		'simple-unless': false,
-		'if-not': true,
-		'private-component': true
+		'attribute-indentation': false,
+		'block-indentation': 1
 	}
 };
