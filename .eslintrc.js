3971da02e7cf2f982f12ca9707b1f5191f265e1a
Jose Luis Represa <joseluis.represa.next@bbva.com>
2021-07-22 11:47:22 +0200

    chore(files): update common files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,60 @@
 'use strict';
 
 module.exports = {
-	root: true,
-	parser: 'babel-eslint',
-	parserOptions: {
-		ecmaVersion: 2018,
-		sourceType: 'module',
-		ecmaFeatures: {
-			legacyDecorators: true,
-		},
-	},
-	plugins: ['ember'],
-	extends: [
-		'eslint:recommended',
-		'plugin:ember/recommended',
-		'plugin:prettier/recommended',
-	],
-	env: {
-		browser: true,
-	},
-	rules: {},
-	overrides: [
-		// node files
-		{
-			files: [
-				'.commitlintrc.js',
-				'.eslintrc.js',
-				'.huskyrc.js',
-				'.prettierrc.js',
-				'.template-lintrc.js',
-				'ember-cli-build.js',
-				'index.js',
-				'testem.js',
-				'blueprints/*/index.js',
-				'config/**/*.js',
-				'tests/dummy/config/**/*.js',
-			],
-			excludedFiles: [
-				'addon/**',
-				'addon-test-support/**',
-				'app/**',
-				'tests/dummy/app/**',
-			],
-			parserOptions: {
-				sourceType: 'script',
-			},
-			env: {
-				browser: false,
-				node: true,
-			},
-			plugins: ['node'],
-			extends: ['plugin:node/recommended'],
-		},
-		{
-			// Test files:
-			files: ['tests/**/*-test.{js,ts}'],
-			extends: ['plugin:qunit/recommended'],
-		},
-	],
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    {
+      files: [
+        '.commitlintrc.js',
+        '.eslintrc.js',
+        '.huskyrc.js',
+        '.prettierrc.js',
+        '.template-lintrc.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
+  ],
 };
