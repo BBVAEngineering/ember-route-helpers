f8394541d985d0143fbae189ff16afb736d9f1eb
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2022-09-28 12:10:35 +0200

    test: ember try update

--- config/ember-try.js
+++ config/ember-try.js
@@ -32,20 +32,7 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
+        name: 'ember-octane',
         env: {
           EMBER_OPTIONAL_FEATURES: JSON.stringify({
             'application-template-wrapper': false,
@@ -55,7 +42,7 @@ module.exports = async function () {
         },
         npm: {
           ember: {
-            edition: 'classic',
+            edition: 'octane',
           },
         },
       },
